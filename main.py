1eebcda8610aa380d4ec62ebb10d64ee20587f4d
goudot <florian.goudot@gmail.com>
2021-03-29 01:58:16 +0200

    add minimalist logging facility

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch
 import time
 import pytz
 import cli.app
+import logging
 
 factor = 0.000001
 bounty_factor = 0.000000001
@@ -16,7 +17,7 @@ def write_pay(item, readable, es):
             "pay_date": pay['timestamp'],
             "Date": readable
         }
-        es.index(index='bounty', body=bounty)
+        logging.debug(es.index(index='bounty', body=bounty))
 
 
 def write_worker(item, readable, es):
@@ -27,7 +28,7 @@ def write_worker(item, readable, es):
             "offline": item[miner]['offline'],
             "Date": readable
         }
-        es.index(index='miner', body=farm)
+        logging.debug(es.index(index='miner', body=farm))
 
 
 def write_global(item, readable, es):
@@ -39,15 +40,17 @@ def write_global(item, readable, es):
         "workersOffline": item['workersOffline'],
         "workersTotal": item['workersTotal']
     }
-    es.index(index='2miner', body=global_info)
+    logging.debug(es.index(index='2miner', body=global_info))
 
 
 def es_entry_point(walletid):
     # Connect to the elastic cluster
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    logging.info(es.info())
 
     while True:
         r = requests.get('https://eth.2miners.com/api/accounts/{}'.format(walletid))
+        logging.info('{} {}'.format(r.status_code, r.url))
         result = r.json()
         readable = datetime.fromtimestamp(result['updatedAt'] * 0.001, pytz.UTC).isoformat()
         write_global(result, readable, es)
@@ -55,14 +58,20 @@ def es_entry_point(walletid):
         write_worker(result['workers'], readable, es)
         time.sleep(10)
 
+def set_log_lvl(log_lvl):
+    if log_lvl == 'INFO':
+        return logging.INFO
+    return logging.debug
 
 @cli.app.CommandLineApp(name='2miner-monitoring')
 def main(app):
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=set_log_lvl(app.params.log_level))
     es_entry_point(app.params.wallet)
 
 
 # Build the options of the CLI
 main.add_param("-w", "--wallet", type=str, help="wallet id", required=True)
+main.add_param("-l", "--log_level", type=str, help="Log Level", default='INFO')
 
 if __name__ == '__main__':
     main.run()
