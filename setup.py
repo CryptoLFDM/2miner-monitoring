94424b951b202e869f0d5562422759926720fca4
Goudot <florian.goudot@gmail.com>
2021-03-30 22:34:25 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='2miner-monitoring',
     version='0.0.1',
-    packages=[''],
+    packages=['2miner-monitoring'],
     url='https://github.com/Tocard/2miner-monitoring',
     license='MIT',
     author='Goudot',
