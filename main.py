e7f95e5ebac152a4ddca44b86976d564ec5dab4a
Goudot <florian.goudot@gmail.com>
2021-03-30 22:12:08 +0200

    add elasticsearch auth

--- main.py
+++ main.py
@@ -61,9 +61,13 @@ def write_stats(item, readable, es, market_price):
     logging.debug(es.index(index='eth_stats', body=stats))
 
 
-def es_entry_point(walletid):
+def es_entry_point(walletid, user, password, es_host, es_port):
     # Connect to the elastic cluster
-    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    es = Elasticsearch(
+        [es_host],
+        http_auth=(user, password),
+        port=es_port,
+    )
     logging.info(es.info())
 
     while True:
@@ -91,11 +95,15 @@ def set_log_lvl(log_lvl):
 @cli.app.CommandLineApp(name='2miner-monitoring')
 def main(app):
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=set_log_lvl(app.params.log_level))
-    es_entry_point(app.params.wallet)
+    es_entry_point(app.params.wallet, app.params.user, app.params.password, app.params.es_host, app.params.es_port)
 
 
 # Build the options of the CLI
 main.add_param("-w", "--wallet", type=str, help="wallet id", required=True)
+main.add_param("-u", "--user", type=str, help="user for es connection")
+main.add_param("-p", "--password", type=str, help="password for es connection")
+main.add_param("-eh", "--es_host", type=str, help="es hosts connection", default='localhost')
+main.add_param("-ep", "--es_port", type=str, help="es port connection", default=9200)
 main.add_param("-l", "--log_level", type=str, help="Log Level", default='INFO')
 
 if __name__ == '__main__':
