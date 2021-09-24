011e21863ab6525c0dbe950f4a8541962d040798
mozquito <contact@ether-source.com>
2021-09-24 12:17:22 +0200

    ship & ready

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='2miner-monitoring',
+    version='0.0.2',
+    packages=['2miner-monitoring'],
+    url='https://github.com/Tocard/2miner-monitoring',
+    license='MIT',
+    author='Mozquito',
+    author_email='contact@ether-source.fr',
+    description='simple 2miner api metrics harvester for es datasource'
+)
